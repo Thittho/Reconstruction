0fe71105573e8c7cc568f875925767e9a364220f
Thittho <tbouchet@maple-irmar.insa-rennes.fr>
2023-12-21 18:49:29 +0100

    changes in invariant decomposition
    Former-commit-id: 2335681549e9f28c406462cfa16d13e4d6f8bc40

--- reconstruction_genus3.m
+++ reconstruction_genus3.m
@@ -1,72 +1,89 @@
-import "Decomposition_genus_3/i11.m" : decompo11, L11, ind11;
-import "Decomposition_genus_3/i12.m" : decompo12, L12, ind12;
-import "Decomposition_genus_3/i13.m" : decompo13, L13, ind13;
-import "Decomposition_genus_3/i21.m" : decompo21, L21, ind21;
-import "Decomposition_genus_3/i22.m" : decompo22, L22, ind22;
-import "Decomposition_genus_3/i23.m" : decompo23, L23, ind23;
-import "Decomposition_genus_3/i31.m" : decompo31, L31, ind31;
-import "Decomposition_genus_3/i32.m" : decompo32, L32, ind32;
-import "Decomposition_genus_3/i33.m" : decompo33, L33, ind33;
-
-import "Decomposition_genus_3/i1111.m" : decompo1111, L1111, ind1111;
-import "Decomposition_genus_3/i1112.m" : decompo1112, L1112, ind1112;
-import "Decomposition_genus_3/i1113.m" : decompo1113, L1113, ind1113;
-import "Decomposition_genus_3/i1122.m" : decompo1122, L1122, ind1122;
-import "Decomposition_genus_3/i1123.m" : decompo1123, L1123, ind1123;
-import "Decomposition_genus_3/i1133.m" : decompo1133, L1133, ind1133;
-import "Decomposition_genus_3/i1222.m" : decompo1222, L1222, ind1222;
-import "Decomposition_genus_3/i1223.m" : decompo1223, L1223, ind1223;
-import "Decomposition_genus_3/i1233.m" : decompo1233, L1233, ind1233;
-import "Decomposition_genus_3/i1333.m" : decompo1333, L1333, ind1333;
-import "Decomposition_genus_3/i2222.m" : decompo2222, L2222, ind2222;
-import "Decomposition_genus_3/i2223.m" : decompo2223, L2223, ind2223;
-import "Decomposition_genus_3/i2233.m" : decompo2233, L2233, ind2233;
-import "Decomposition_genus_3/i2333.m" : decompo2333, L2333, ind2333;
-import "Decomposition_genus_3/i3333.m" : decompo3333, L3333, ind3333;
-
-function Prod(L1, L2)
-    return &*[L2[i] : i in L1];
+function DOBis(f)
+    inv := DixmierOhnoInvariants(f);
+    if Characteristic(Universe(inv)) in {19, 47, 277, 523} then inv[3] +:= inv[4]; end if;
+    return inv;
 end function;
 
-function evaluation(decompo, L, ind, inv)
-    m := #L;
-    n := #decompo;
-    res := &+[decompo[i]*Prod(L[ind[i]], inv) : i in [1..n] | ind[i] le m];
-    return res;
-end function;
+load "decompo.m";
 
 function ReconstructionFromDixmierOhnoBis(inv : Minimize := false)
-    _<X,Y,Z> := PolynomialRing(Rationals(), 3);
+
+    if Characteristic(Universe(inv)) in {2, 3, 5, 7, 17} then "Not implemented for char 2, 3, 5, 7 or 17 "; return 0; end if;
+
+    M := Matrix([[P11(inv), P12(inv), P13(inv)],
+                 [P21(inv), P22(inv), P23(inv)],
+                 [P31(inv), P32(inv), P33(inv)]]);
+    _<X,Y,Z> := PolynomialRing(FieldOfFractions(Universe(inv)), 3);
     
-    f := evaluation(decompo1111, L1111, ind1111, inv)*X^4+
-    4*evaluation(decompo1112, L1112, ind1112, inv)*X^3*Y+
-    4*evaluation(decompo1113, L1113, ind1113, inv)*X^3*Z+
-    6*evaluation(decompo1122, L1122, ind1122, inv)*X^2*Y^2+
-    12*evaluation(decompo1123, L1123, ind1123, inv)*X^2*Y*Z+
-    6*evaluation(decompo1133, L1133, ind1133, inv)*X^2*Z^2+
-    4*evaluation(decompo1222, L1222, ind1222, inv)*X*Y^3+
-    12*evaluation(decompo1223, L1223, ind1223, inv)*X*Y^2*Z+
-    12*evaluation(decompo1233, L1233, ind1233, inv)*X*Y*Z^2+
-    4*evaluation(decompo1333, L1333, ind1333, inv)*X*Z^3+
-    evaluation(decompo2222, L2222, ind2222, inv)*Y^4+
-    4*evaluation(decompo2223, L2223, ind2223, inv)*Y^3*Z+
-    6*evaluation(decompo2233, L2233, ind2233, inv)*Y^2*Z^2+
-    4*evaluation(decompo2333, L2333, ind2333, inv)*Y*Z^3+
-    evaluation(decompo3333, L3333, ind3333, inv)*Z^4;
+    f := P1111(inv)*X^4+
+        4*P1112(inv)*X^3*Y+
+        4*P1113(inv)*X^3*Z+
+        6*P1122(inv)*X^2*Y^2+
+        12*P1123(inv)*X^2*Y*Z+
+        6*P1133(inv)*X^2*Z^2+
+        4*P1222(inv)*X*Y^3+
+        12*P1223(inv)*X*Y^2*Z+
+        12*P1233(inv)*X*Y*Z^2+
+        4*P1333(inv)*X*Z^3+
+        P2222(inv)*Y^4+
+        4*P2223(inv)*Y^3*Z+
+        6*P2233(inv)*Y^2*Z^2+
+        4*P2333(inv)*Y*Z^3+
+        P3333(inv)*Z^4;
     
-    if Minimize then
-        return MinRedTernaryForm(f);
+    if Determinant(M) ne 0 then
+        if Minimize then
+            f1 := MinRedTernaryForm(f);
+            return f1;
+        end if;
+        return f;
     end if;
     
-    return f;
+    inv1 := DOBis(f);
+    if DixmierOhnoInvariantsEqual(inv, inv1) then
+        return f;
+    end if;
+    
+    "Not a basis, not implemented yet.";
+    
+    return 0;
 end function;
 
+procedure Reconstruction(F, n : a := 10, b := 10^2)
+    R<x,y,z> := PolynomialRing(F, 3);
+    L4 := MonomialsOfDegree(R, 4);
+    if IsFinite(F) then
+        forms := [&+[Random(F)*l : l in L4] : i in [1..10]];
+    else
+        forms := [&+[Random(a,b)*l : l in L4] : i in [1..10]];
+    end if;
 
-// Random example
+    for f in forms do
+        inv := DOBis(f);
 
-_<x,y,z> := PolynomialRing(Rationals(), 3);
-f := Random(-5, 5)*x^4+Random(-5,5)*x^3*y+Random(-5,5)*x^3*z+Random(-5,5)*y^3*x+Random(-5,5)*y^3*z+Random(-5,5)*z^3*x+Random(-5,5)*z^3*y+Random(-5,5)*x^2*y^2+Random(-5,5)*x^2*z^2+Random(-5,5)*y^2*z^2+Random(-5,5)*x^2*y*z+Random(-5,5)*y^2*x*z+Random(-5,5)*z^2*x*y+Random(-5,5)*y^4+Random(-5,5)*z^4;
+        time f1 := ReconstructionFromDixmierOhnoBis(inv); // Reconstruction
+        I3,I6,I9,J9,I12,J12,I15,J15,I18,J18,I21,J21,I27 := Explode(inv);
+        
+        if f1 ne 0 then
+            f1 := R!(f1);
+            inv2 := DOBis(f1);
+            DixmierOhnoInvariantsEqual(inv, inv2); // Check that the reconstructed form has the same invariants
+        end if;
+    end for;
+end procedure;
 
-inv := DixmierOhnoInvariants(f);
+// Reconstruction over a big field
+K := GF(NextPrime(10^200)^10);
+Reconstruction(K, 5);
 
-f1 := ReconstructionFromDixmierOhnoBis(inv);
+// Over a small field
+K := GF(41);
+Reconstruction(K, 5);
+
+// Over Q
+K := Rationals();
+Reconstruction(K, 5 : a := 10^5, b := 10^10);
+
+// Example given in your article
+ReconstructionFromDixmierOhnoBis([0,0,0,0,-7*19/(2^14*3^8*5^2),0,-11*19/(2^17*3^10*5^2),0,7*19^2/(2^20*3^11*5^3),19^2/(2^20*3^11*5^3),-19^2*31/(2^24*3^13*5^5),-17*19^2/(2^21*3^12*5^5),-19^2*6553/(2^39*3^6*5^5*11)]);
+PlaneQuarticFromDixmierOhnoInvariants([0,0,0,0,-7*19/(2^14*3^8*5^2),0,-11*19/(2^17*3^10*5^2),0,7*19^2/(2^20*3^11*5^3),19^2/(2^20*3^11*5^3),-19^2*31/(2^24*3^13*5^5),-17*19^2/(2^21*3^12*5^5),-19^2*6553/(2^39*3^6*5^5*11)] : minimize := false);
