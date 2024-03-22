9772e6d1be2005ba070c723eb34711467ec3cef2
Thittho <95592680+Thittho@users.noreply.github.com>
2024-03-22 14:38:46 +0100

    Update reconstruction_genus3.m
    Former-commit-id: e509cbc782ea0d7bd42aa2655f8baf29722135e1

--- reconstruction_genus3.m
+++ reconstruction_genus3.m
@@ -6,7 +6,14 @@ end function;
 
 load "decompo.m";
 
-function ReconstructionFromDixmierOhnoBis(inv : Minimize := false)
+intrinsic ReconstructionFromDixmierOhnoBis(inv::SeqEnum : 
+    minimize := false) -> RngMPolElt
+{
+Reconstruct a ternary quartic from a generic tuple of Dixmier-Ohno invariants. 
+It is not implemented for characteristic 2, 3, 5, 7 or 17.
+
+If minimize is set to true, the coefficients of the ternary quartic are minimized using Elsenhans and Stoll's method MinRedTernaryForm.
+}
 
     if Characteristic(Universe(inv)) in {2, 3, 5, 7, 17} then "Not implemented for char 2, 3, 5, 7 or 17 "; return 0; end if;
 
@@ -32,7 +39,7 @@ function ReconstructionFromDixmierOhnoBis(inv : Minimize := false)
         P3333(inv)*Z^4;
     
     if Determinant(M) ne 0 then
-        if Minimize then
+        if minimize then
             f1 := MinRedTernaryForm(f);
             return f1;
         end if;
@@ -48,42 +55,3 @@ function ReconstructionFromDixmierOhnoBis(inv : Minimize := false)
     
     return 0;
 end function;
-
-procedure Reconstruction(F, n : a := 10, b := 10^2)
-    R<x,y,z> := PolynomialRing(F, 3);
-    L4 := MonomialsOfDegree(R, 4);
-    if IsFinite(F) then
-        forms := [&+[Random(F)*l : l in L4] : i in [1..10]];
-    else
-        forms := [&+[Random(a,b)*l : l in L4] : i in [1..10]];
-    end if;
-
-    for f in forms do
-        inv := DOBis(f);
-
-        time f1 := ReconstructionFromDixmierOhnoBis(inv); // Reconstruction
-        I3,I6,I9,J9,I12,J12,I15,J15,I18,J18,I21,J21,I27 := Explode(inv);
-        
-        if f1 ne 0 then
-            f1 := R!(f1);
-            inv2 := DOBis(f1);
-            DixmierOhnoInvariantsEqual(inv, inv2); // Check that the reconstructed form has the same invariants
-        end if;
-    end for;
-end procedure;
-
-// Reconstruction over a big field
-K := GF(NextPrime(10^200)^10);
-Reconstruction(K, 5);
-
-// Over a small field
-K := GF(41);
-Reconstruction(K, 5);
-
-// Over Q
-K := Rationals();
-Reconstruction(K, 5 : a := 10^5, b := 10^10);
-
-// Example given in your article
-ReconstructionFromDixmierOhnoBis([0,0,0,0,-7*19/(2^14*3^8*5^2),0,-11*19/(2^17*3^10*5^2),0,7*19^2/(2^20*3^11*5^3),19^2/(2^20*3^11*5^3),-19^2*31/(2^24*3^13*5^5),-17*19^2/(2^21*3^12*5^5),-19^2*6553/(2^39*3^6*5^5*11)]);
-PlaneQuarticFromDixmierOhnoInvariants([0,0,0,0,-7*19/(2^14*3^8*5^2),0,-11*19/(2^17*3^10*5^2),0,7*19^2/(2^20*3^11*5^3),19^2/(2^20*3^11*5^3),-19^2*31/(2^24*3^13*5^5),-17*19^2/(2^21*3^12*5^5),-19^2*6553/(2^39*3^6*5^5*11)] : minimize := false);
